39cbe79a1b4cfae48e96aca4f03820c58b4b95fa
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-06 02:18:35 +0600

    added Aggregate.js

--- Mission-1/Module-2/Aggregate.js
+++ Mission-1/Module-2/Aggregate.js
@@ -1,16 +1,41 @@
 const surveyData = ["A", "A", "C", "C", "A", "A", "B", "B", "B", "D", "A"];
 
-const count = surveyData.reduce((table, response) => {
-//   console.log(table,response);
-
-  if (table[response]) {
-    table[response] = table[response] + 1;
+const count = surveyData.reduce((accmulatore, data) => {
+  //   console.log(table,response);
+  if (accmulatore[data]) {
+    accmulatore[data] = accmulatore[data] + 1;
   } else {
-    table[response] = 1;
+    accmulatore[data] = 1;
   }
 
-  return table;
+  return accmulatore;
 }, {});
 
-console.log(count);
+// console.log(count);
 
+const sales = [
+  { category: "Electronic", item: "Laptop", price: "1200", quantity: 1 },
+  { category: "Books", item: "Next js", price: "120", quantity: 2 },
+  { category: "Electronic", item: "Mouse", price: "100", quantity: 2 },
+  { category: "Home", item: "Chair", price: "200", quantity: 1 },
+  { category: "Books", item: "Node js", price: "200", quantity: 1 },
+  { category: "Electronic", item: "Keyboard", price: "200", quantity: 2 },
+];
+
+const totalSalesByCategory = sales.reduce((acc, sale) => {
+  const { category, price, quantity } = sale;
+  if (!acc[category]) {
+    acc[category] = {
+      totalRevenue: 0,
+      itemCount: 0,
+    };
+  }
+
+acc[category].totalRevenue+=quantity;
+acc[category].itemCount+=quantity
+
+return acc
+}, {});
+
+
+console.log(totalSalesByCategory);
