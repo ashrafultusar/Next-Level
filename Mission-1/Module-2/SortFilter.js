4579f17f95e40fea87e321c0362e652c9b4726cc
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-05 17:01:00 +0600

    added all

--- Mission-1/Module-2/SortFilter.js
+++ Mission-1/Module-2/SortFilter.js
@@ -4,16 +4,24 @@
 
 // number.sort((a,b)=>a-b)
 
-
-
-// nested arry ke one array te convert korte 
-const arr=[1,2,3,5,[7,9,]]
-const arr1=[1,2,3,5,[7,9,[11,23,45,[12,21]]]]
+// nested arry ke one array te convert korte
+const arr = [1, 2, 3, 5, [7, 9]];
+const arr1 = [1, 2, 3, 5, [7, 9, [11, 23, 45, [12, 21]]]];
 
 // single nested thakle
-const flatArr=arr.flat()
-console.log(flatArr);
+const flatArr = arr.flat();
+// console.log(flatArr);
 
 // multiple nested thakle
-const flatArr1=arr1.flat(Infinity)
-console.log(flatArr1);
+const flatArr1 = arr1.flat(Infinity);
+// console.log(flatArr1);
+
+
+// Array.from()
+const range = (start, stop, step) => {
+   return Array.from(
+    { length: Math.ceil((stop - start) / step) },
+    (_, i) => start + i * step
+  );
+};
+console.log(range(1,10,4));
