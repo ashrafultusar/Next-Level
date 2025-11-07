9abe18a0d62efde736c7a59abd1b6a15776d9f8b
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-11-08 01:47:07 +0600

    added all

---
+++ Mission-1/Module-3/Stack.js
@@ -0,0 +1,46 @@
+class Stack {
+  constructor() {
+    this.item = [];
+  }
+
+  push(value) {
+    this.item.push(value);
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.item.pop();
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.item[this.item.length - 1];
+  }
+
+  isEmpty() {
+    return this.item.length === 0;
+  }
+
+  print() {
+    console.log(this.item.slice().reverse().join("->"));
+  }
+}
+
+const stack = new Stack();
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+stack.print();
+console.log(stack.peek());
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+
